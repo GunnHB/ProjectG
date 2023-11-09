62aaba776e53b5a161387a36d1555a051f9f64d0
GunnHB <gunn0467@gmail.com>
2023-11-09 12:57:29 +0900

    [11/09] 데이터 저장 기능 구현

--- Assets/Scripts/UI/HUD/SelectCharacterHUD.cs
+++ Assets/Scripts/UI/HUD/SelectCharacterHUD.cs
@@ -11,100 +11,43 @@ using System;
 
 public class SelectCharacterHUD : UIHUDBase
 {
-    // private const string LeftGroup = "LeftGroup";
-    // private const string CenterGroup = "CetnerGroup";
-    // private const string RightGroup = "RightGroup";
-
-    // [TabGroup(LeftGroup), SerializeField] private TextMeshProUGUI _leftPlayerName;
-    // [TabGroup(LeftGroup), SerializeField] private GameObject _leftContainGroup;
-    // [TabGroup(LeftGroup), SerializeField] private GameObject _leftButtonGroup;
-    // [TabGroup(LeftGroup), SerializeField] private Button _leftCreateButton;
-    // [TabGroup(LeftGroup), SerializeField] private Button _leftDeleteButton;
-    // [TabGroup(LeftGroup), SerializeField] private Button _leftPlayButton;
-
-    // [TabGroup(CenterGroup), SerializeField] private TextMeshProUGUI _centerPlayerName;
-    // [TabGroup(CenterGroup), SerializeField] private GameObject _centerContainGroup;
-    // [TabGroup(CenterGroup), SerializeField] private GameObject _centerButtonGroup;
-    // [TabGroup(CenterGroup), SerializeField] private Button _centerCreateButton;
-    // [TabGroup(CenterGroup), SerializeField] private Button _centerDeleteButton;
-    // [TabGroup(CenterGroup), SerializeField] private Button _centerPlayButton;
-
-    // [TabGroup(RightGroup), SerializeField] private TextMeshProUGUI _rightPlayerName;
-    // [TabGroup(RightGroup), SerializeField] private GameObject _rightContainGroup;
-    // [TabGroup(RightGroup), SerializeField] private GameObject _rightButtonGroup;
-    // [TabGroup(RightGroup), SerializeField] private Button _rightCreateButton;
-    // [TabGroup(RightGroup), SerializeField] private Button _rightDeleteButton;
-    // [TabGroup(RightGroup), SerializeField] private Button _rightPlayButton;
-
-    [Title("[ObjectPool]"), SerializeField] private ObjectPool _slotPool;
-
-    [Title("[Back Button]"), SerializeField] private Button _backButton;
+    [Title("[ObjectPool]")]
+    [SerializeField] private ObjectPool _slotPool;
+    [Title("[Back Button]")]
+    [SerializeField] private Button _backButton;
 
     protected override void Awake()
     {
         base.Awake();
 
-        // // left
-        // Util.AddButtonListener(_leftCreateButton, () => OnClickCreate(0));
-        // Util.AddButtonListener(_leftDeleteButton, () => OnClickDelete(0));
-        // Util.AddButtonListener(_leftPlayButton, () => OnClickPlay(0));
-
-        // // right
-        // Util.AddButtonListener(_rightCreateButton, () => OnClickCreate(1));
-        // Util.AddButtonListener(_rightDeleteButton, () => OnClickDelete(1));
-        // Util.AddButtonListener(_rightPlayButton, () => OnClickPlay(1));
-
-        // // center
-        // Util.AddButtonListener(_centerCreateButton, () => OnClickCreate(2));
-        // Util.AddButtonListener(_centerDeleteButton, () => OnClickDelete(2));
-        // Util.AddButtonListener(_centerPlayButton, () => OnClickPlay(2));
-
+        // 슬롯 풀 초기화
         _slotPool.Initialize();
 
         // back button
         Util.AddButtonListener(_backButton, OnClickBackButton);
-
-        // // 일단 다 끄기
-        // DeactiveContainGroup();
-        // DeactiveButtonGroup();
-
-        // // 일단 다 켜기
-        // ActiveCreateButton();
     }
-
-    // private void DeactiveContainGroup()
-    // {
-    //     _leftContainGroup.SetActive(false);
-    //     _centerContainGroup.SetActive(false);
-    //     _rightContainGroup.SetActive(false);
-    // }
-
-    // private void DeactiveButtonGroup()
-    // {
-    //     _leftButtonGroup.SetActive(false);
-    //     _centerButtonGroup.SetActive(false);
-    //     _rightButtonGroup.SetActive(false);
-    // }
-
-    // private void ActiveCreateButton()
-    // {
-    //     _leftCreateButton.gameObject.SetActive(true);
-    //     _centerCreateButton.gameObject.SetActive(true);
-    //     _rightCreateButton.gameObject.SetActive(true);
-    // }
 
     public void Init()
     {
+        // 기존에 Dequeue 했던 것들 다시 가져오기
         _slotPool.ReturnAllObject();
 
+        // json 배열 길이만큼 슬롯 생성
         for (int index = 0; index < JsonManager.Instance.BaseSlotData._isEmpty.Length; index++)
-            _slotPool.GetObject();
+        {
+            var item = _slotPool.GetObject();
+            var isEmpty = JsonManager.Instance.BaseSlotData._isEmpty[index];
+
+            item.name = $"SaveSlot_{index}";
+            item.SetActive(true);
+
+            if (item.TryGetComponent(out SelectSlot slot))
+                slot.InitSlot((SelectSlot.SeparateType)index, isEmpty, ClosePanel);
+        }
     }
 
-    private void OnClickCreate(int index)
+    private void ClosePanel()
     {
-        GameManager.Instance.SetSelectedSlotIndex(index);
-
         this.gameObject.SetActive(false);
 
         var openUI = UIManager.Instance.FindOpendUI<CustomizeHUD>(UIManager.Instance.HudCanvas);
@@ -115,16 +58,6 @@ public class SelectCharacterHUD : UIHUDBase
             openUI = UIManager.Instance.OpenUI<CustomizeHUD>("HUD/CustomizeHUD");
 
         openUI.Init();
-    }
-
-    private void OnClickDelete(int index)
-    {
-        GameManager.Instance.SetSelectedSlotIndex(index);
-    }
-
-    private void OnClickPlay(int index)
-    {
-        GameManager.Instance.SetSelectedSlotIndex(index);
     }
 
     private void OnClickBackButton()
--- Assets/Scripts/UI/HUD/SelectCharacterHUD/SelectSlot.cs
+++ Assets/Scripts/UI/HUD/SelectCharacterHUD/SelectSlot.cs
@@ -7,18 +7,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 using Sirenix.OdinInspector;
+using UnityEngine.Events;
 
 public class SelectSlot : MonoBehaviour
 {
     public enum SeparateType
     {
-        None,
         Left,
         Center,
         Right,
     }
-
-    private SeparateType _currentType;
 
     [Title("[Text]")]
     [SerializeField] private TextMeshProUGUI _playerName;
@@ -32,8 +30,68 @@ public class SelectSlot : MonoBehaviour
     [SerializeField] private Button _deleteButton;
     [SerializeField] private Button _playButton;
 
-    public void InitSlot(SeparateType type)
+    private SeparateType _currentType;
+    private UnityAction _closePanelAction;
+
+    private bool _emptyState = false;
+
+    public void InitSlot(SeparateType type, bool isEmpty, UnityAction callback)
     {
+        ResetData();
+
         _currentType = type;
+        _emptyState = isEmpty;
+        _closePanelAction = callback;
+
+        SetSlotState();
+        SetButtonListener();
+
+        SetPlayerNameText();
+    }
+
+    private void SetSlotState()
+    {
+        _containGroup.SetActive(!_emptyState);
+        _buttonGroup.SetActive(!_emptyState);
+
+        _createButton.gameObject.SetActive(_emptyState);
+    }
+
+    private void SetPlayerNameText()
+    {
+        // 빈 슬롯이면 실행할 필요 없음
+        if (_emptyState)
+            return;
+
+        _playerName.text = JsonManager.Instance.BaseData._playerName[(int)_currentType];
+    }
+
+    private void SetButtonListener()
+    {
+        Util.AddButtonListener(_createButton, OnClickCreateButton);
+        Util.AddButtonListener(_deleteButton, OnClickDeleteButton);
+        Util.AddButtonListener(_playButton, OnClickPlayButton);
+    }
+
+    private void OnClickCreateButton()
+    {
+        GameManager.Instance.SetSelectedSlotIndex((int)_currentType);
+
+        _closePanelAction?.Invoke();
+    }
+
+    private void OnClickDeleteButton()
+    {
+
+    }
+
+    private void OnClickPlayButton()
+    {
+
+    }
+
+    private void ResetData()
+    {
+        _closePanelAction = null;
     }
 }
