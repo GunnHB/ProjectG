36bb72626c6322296e4b23d4d0b6f0e0a98bd224
GunnHB <gunn0467@gmail.com>
2023-11-15 13:02:03 +0900

    [11/15] 인벤토리 시스템
    #누락 내용 커밋

--- Assets/Scripts/Json/PlayerData/PlayerInventoryData.cs
+++ Assets/Scripts/Json/PlayerData/PlayerInventoryData.cs
@@ -8,10 +8,14 @@ public class PlayerInventoryData : MonoBehaviour
 {
     // 인벤토리는 가변형이라 리스트
     private Dictionary<SlotIndex, List<Item.Data>> _playerInventory;
+    private Dictionary<SlotIndex, int> _playerGold;
 
     public PlayerInventoryData()
     {
         for (int index = 0; index < GameValue.SAVE_SLOT_COUNT; index++)
+        {
             _playerInventory.Add((SlotIndex)index, new());
+            _playerGold.Add((SlotIndex)index, 0);
+        }
     }
 }
