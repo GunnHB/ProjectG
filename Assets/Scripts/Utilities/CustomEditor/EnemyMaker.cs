7fa0438e30625fbb629333ea1aee30e41e084b27
GunnHB <gunn0467@gmail.com>
2023-12-27 17:07:38 +0900

    [12/27] EnemyMaker 추가
    #진행 중

---
+++ Assets/Scripts/Utilities/CustomEditor/EnemyMaker.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+
+using UnityEngine;
+
+using Sirenix.OdinInspector.Editor;
+
+public class EnemyData
+{
+    public long _id;
+    public string _name;
+}
+
+public class EnemyMaker : OdinMenuEditorWindow
+{
+    [MenuItem("Tools/CustomEditors/EnemyMaker")]
+    private static void Open()
+    {
+        GetWindow<EnemyMaker>();
+    }
+
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        throw new System.NotImplementedException();
+        var tree = new OdinMenuTree();
+
+        // tree.AddAllAssetsAtPath("Enemy Data", "")
+    }
+}
