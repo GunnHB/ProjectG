7769358509c10c64d5aacc9c1ce8b72cc93b0816
GunnHB <gunn0467@gmail.com>
2024-01-15 20:52:52 +0900

    [01/15] 인벤토리 시스템 리팩토링

---
+++ Assets/Scripts/Item/ItemBaseV2.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class ItemDataV2
+{
+
+}
+
+public class ItemBaseV2 : MonoBehaviour
+{
+    [SerializeField]
+    private ItemDataV2 _data;
+}
